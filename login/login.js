4d40386597ac0bb9069fc548aecb9bdd7fe3d5ce
franco211 <francisbyaruhanga88@gmail.com>
2023-01-20 00:21:01 -0500

    git commit

--- login/login.js
+++ login/login.js
@@ -96,6 +96,88 @@ form.addEventListener("submit", function (event) {
     });
 });
 
+// Send a validation email to the user when they sign up
+function sendValidationEmail(email) {
+  // Generate a unique code or link to include in the email
+  const validationCode = uuidv4();
+
+  // Send the email with the validation code/link using nodemailer
+  transport.sendMail({
+    to: email,
+    subject: "Email Validation Code",
+    html: `Please click <a href="http://localhost:3000/verify?email=${email}&code=${validationCode}">here</a> to confirm your email address.`,
+  });
+
+  // Save the validation code in the database associated with the user's email
+  db.get("validationCodes").push({ email, code: validationCode }).write();
+}
+// Verify the user's email address when they click on the validation link
+function verifyEmail(email, validationCode) {
+  // Look up the validation code in the database
+  const savedCode = db
+    .get("validationCodes")
+    .find({ email, code: validationCode })
+    .value();
+
+  if (savedCode) {
+    // Mark the user's account as verified
+    db.get("users").find({ email }).assign({ isVerified: true }).write();
+
+    // Delete the validation code from the database
+    db.get("validationCodes").remove({ email, code: validationCode }).write();
+    return true;
+  } else {
+    return false;
+  }
+}
+// In your login function, check if the user's account is verified before allowing login
+function login() {
+  // Get the form data
+  const form = document.getElementById("login-form");
+  const formData = new FormData(form);
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  // Perform validation
+  if (!email || !password) {
+    alert("All fields are required");
+    return;
+  }
+
+  // Check if the user exists in the database
+  const user = db.get("users").find({ email }).value();
+  if (!user) {
+    alert("Incorrect email or password.");
+    return;
+  }
+
+  // Check if the account is verified
+  if (!user.isVerified) {
+    alert("Please verify your email address before logging in.");
+    return;
+  }
+
+  // Verify the password
+  const hashedPassword = crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("hex");
+  if (hashedPassword !== user.password) {
+    alert("Incorrect email or password.");
+    return;
+  }
+
+  // Login successful
+  alert("Login successful!");
+}
+
+// Listen for the submit event on the login form
+const loginForm = document.getElementById("login-form");
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the form from submitting
+  login();
+});
+
 document.getElementById("my-button").style.backgroundColor = "blue";
 
 document.getElementById("my-button").addEventListener("click", function () {
